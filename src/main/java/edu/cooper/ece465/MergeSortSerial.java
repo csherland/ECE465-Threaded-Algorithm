fc3085757cfe05984ad234913acf772e687d9d47
Christian Sherland <christiansherland@gmail.com>
2014-02-06 11:26:59 -0500

    Cleaned up the serial sort

--- src/main/java/edu/cooper/ece465/MergeSortSerial.java
+++ src/main/java/edu/cooper/ece465/MergeSortSerial.java
@@ -1,3 +1,19 @@
+/*
+ * Authors: Christian Sherland
+ *          Ethan Lusterman
+ *          Michael Scibor
+ *          Elli Rappaport
+ *
+ * Date:    2/6/14
+ *
+ * MergeSortSerial.java
+ *      Takes lits of integer inputs from test.txt and applies
+ *      the merge sort algorithm to the data
+ *  
+ *      The sorted result is written to outSerial.txt
+ *
+ */
+
 package edu.cooper.ece465;
 
 import java.util.*;
@@ -8,48 +24,48 @@ public class MergeSortSerial {
 
     public static void main(String[] args) {
 
-        List <Integer> myIntegers = new ArrayList<Integer>();
+        List<Integer> input = new ArrayList<Integer>();
 
         // Read input
-        BufferedReader br = null;
         try {
-            String fromFile;
-            br = new BufferedReader(new FileReader("test.txt"));
-            
-            while ((fromFile = br.readLine()) != null) {
-                int toAdd = Integer.parseInt(fromFile);
-                myIntegers.add(toAdd);
+            String currentInput;
+            BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+         
+            while ((currentInput = br.readLine()) != null) {
+                input.add(Integer.parseInt(currentInput));
             }
 
         } catch (IOException e) {
-            System.out.println("File not found.");
+            System.out.println("Error: input file not found");
         }
 
-        // Sort the list
+    
+        // Need to convert to an int array
+        Integer[] inputArray = input.toArray(new Integer[input.size()]);
+        int[] sortArray = new int[inputArray.length];
+        for (int i = 0; i < inputArray.length; ++i) {
+            sortArray[i] = inputArray[i].intValue();
+        }
+    
+        // Sort the input
         MergeSort m = new MergeSort();
-        Integer[] myIntArray = myIntegers.toArray(new Integer[myIntegers.size()]);
+        m.sort(sortArray);
 
-        int[] myInts = new int[myIntArray.length];
-        for (int i = 0; i < myIntArray.length; ++i) {
-            myInts[i] = myIntArray[i].intValue();
-        }
-        m.sort(myInts);
-
-        // Write the output
-        BufferedWriter outputWriter = null; 
+        // Write the output to file
         try {
-            outputWriter = new BufferedWriter(new FileWriter("out.txt"));
-            for (int i = 0; i < myInts.length; i++) {
-                outputWriter.write(Integer.toString(myInts[i]));
+            BufferedWriter outputWriter = new BufferedWriter(new FileWriter("outSerial.txt"));
+            
+            for (int i = 0; i < sortArray.length; i++) {
+                outputWriter.write(Integer.toString(sortArray[i]));
                 outputWriter.newLine();
             }
-            
-            outputWriter.flush();  
+
+            outputWriter.flush(); 
             outputWriter.close();
-        
+
         } catch (IOException e) {
-            System.out.println("Error writing file");
+            System.out.println("Error: write error");
         }
-        
+ 
     }
 }
